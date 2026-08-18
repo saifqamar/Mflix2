8bfd34293ce14c5bdd01671cb8c6ef102a60c268
saif <Mubashir>
2026-08-18 13:53:04 +0500

    prepared frontend for remote deployment

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:3000";
+const API_BASE_URL = "https://empathetic-empathy-production-ad60.up.railway.app";
 
 const suggestBtn = document.getElementById("suggest-btn");
 
