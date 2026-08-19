5e2c74de1ac600785fe6b9d9a762badbe4112365
saif <Mubashir>
2026-08-19 17:26:17 +0500

    new new cor

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const fastify = Fastify({
 
 // cors
 fastify.register(cors, {
-  origin: ["http://localhost:3000", "http://192.168.18.18:3000", "https://aesthetic-biscotti-4fe88a.netlify.app"],
+  origin: ["http://localhost:3000", "http://192.168.18.18:3000", "https://aesthetic-biscotti-4fe88a.netlify.app", "http://localhost:44273"],
 });
 
 // ============================================
