e37132b8d6ce1dc57ea2e202b1e97bd57c851a74
saif <Mubashir>
2026-08-19 17:02:12 +0500

    url frontend fix

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:3000";
+const API_BASE_URL = "https://ample-gentleness-production-f104.up.railway.app";
 
 const suggestBtn = document.getElementById("suggest-btn");
 
